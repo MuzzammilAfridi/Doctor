c5e5a35a8d6e83cf3506220f5a51979be7ea80ba
Muzzammil Afridi <muzzammilafridi1319@gmail.com>
2024-01-13 12:37:55 +0530

    Home is Done

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 const Nav = ({navItem}) => {
   return (
     <div className='h-[15vh] px-5'>
-        <ul className='h-full flex items-center justify-end gap-10'>
-          {navItem.map((item)=>(
-              <li><a className='text-xl font-semibold' href="#">{item}</a></li>
-          ))}
+        <ul className='h-full flex items-center  justify-end gap-10'>
+          
+              <li><a className='text-xl hover:text-red-700 text-[#16A34A] font-semibold '  href="#">Home</a></li>
+              <li><a className='text-xl hover:text-red-700 '  href="#">Services</a></li>
+              <li><a className='text-xl hover:text-red-700 '  href="#">About us</a></li>
+              <li><a className='text-xl hover:text-red-700 '  href="#">Contact us</a></li>
+      
           
            
         </ul>
