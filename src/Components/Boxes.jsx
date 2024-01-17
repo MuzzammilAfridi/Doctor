de96d640b3ded605154a33a0020df4d1ce1d6d31
Muzzammil Afridi <muzzammilafridi1319@gmail.com>
2024-01-17 18:20:22 +0530

    This is Boxes Components

---
+++ src/Components/Boxes.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Boxes = () => {
+  return (
+    <div className='h-fit mt-10 flex gap-5 justify-center items-center w-screen '>
+      <div className="h-[100px] w-fit bg-white rounded-xl text-center flex flex-col items-center justify-center px-9 text-black">
+       <h2 className=' text-2xl font-semibold'><span className='text-green-600 '>20+ <br /></span> Doctors</h2>
+      
+      </div>
+      <div className="h-[100px] w-fit bg-white rounded-xl text-center flex flex-col items-center justify-center px-9 text-black">
+        <h2 className='text-2xl font-semibold'><span className='text-green-600'>50,000+ <br /></span>Customer</h2>
+      </div>
+      <div className="h-[100px] w-fit bg-white rounded-xl text-center flex flex-col items-center justify-center px-9 text-black">
+        <h2 className='text-2xl font-semibold'><span className='text-green-600'>500+ <br />
+</span>Student</h2>
+      </div>
+
+    </div>
+  )
+}
+
+export default Boxes
